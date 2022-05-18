db007f9b31e4a761c09854faf37641931e998453
Tim Huang <tim.huang20@hotmail.com>
2022-05-18 14:27:57 +1000

    Finishing voronoi segmentation correction

--- src/stage_one/image.py
+++ src/stage_one/image.py
@@ -291,7 +291,7 @@ def correct_segmentation(labelImg, origCellImg, nucleiImage, newLabelImg):
             print('')
         elif (len(centroidList) > 2):
             segmentedImg = voronoi_seg(centroidList, cellImg)
-            correct_voronoi(segmentedImg, nucleiBox, cellImg)
+            segmentedImg = correct_voronoi(segmentedImg, nucleiBox)
                 
         # Add the label to the new labelled image.
         boundBox = newLabelImg[bound[0]:bound[2], bound[1]:bound[3]]
@@ -310,94 +310,129 @@ def correct_segmentation(labelImg, origCellImg, nucleiImage, newLabelImg):
         # REGIONS CORRESPOND TO DIFF CELLS. NOTE THAT THIS SHOULD NOT RUN ON A CELL
         # WITH ONE NUCLEUS.
 
-def correct_voronoi(segmentedImg, nucleiBox, cellImg):
-    # Then, if there are regions with the same label that are not adjacent,
-    # try to find the region it was originally connected to.
-    # First, distinguish the regions that should not be part of the same label
-    # i.e. labels that are not connected to the nucleus.
-    # Then, connect these regions to any other voronoi region, by obtaining
-    # their regions in a numpy array. Also collect the label '-1', as this
-    # is the voronoi ridge separating the regions.
-    # Then set all of these regions to the same label, and use measure.label
-    # once again. If the number of resulting regions is 1, this indicates
-    # that the regions are connecting.
-    
-    # Now, correct the voronoi segmentation. There will be some labels that
-    # have regions that are not connected spatially. Therefore, the correction
-    # will attempt to find the 'correct' connections for these regions.
-    
-    # First, obtain the regions that are not meant to be in this voronoi region.
-    # i.e. regions that are not connected to the nuclei.
-    # First, for each voronoi region, create an intensity image. Then, the
-    # corresponding nucleus for this voronoi region was obtained.
-
-    # Obtain nuclei that are completely within a cell... Obtain the nucleus with the
-    # highest intensity.
-    #   - Obtain the intensity image of segmentedImg
-    # Then obtain the regions that do not include this nuclus
-    #   - Done by: Removing the region that contains this nuclei (> 0 intensity)
-    newImg = np.zeros(segmentedImg.shape)
+# This function is called after a voronoi segmentation. This is to fix the segmentation,
+# wherein a voronoi region may contain labels that are not adjacent to each other. This
+# indicates that the region is actually connected to another voronoi region.
+# Therefore, this function facilitates this reconnection. A while loop will call
+# to another function, which will progressively correct the segmented image after
+# each pass, by connecting a non-connected voronoi segment to different voronoi region.
+# Arguments:
+#   - segmentedImg (numpy array): The voronoi segmented image.
+#   - nucleiBox (numpy array): The image of the nuclei for the image.
+# Returns:
+#   - segmentedImg (numpy array): The corrected voronoi segmented image.
+def correct_voronoi(segmentedImg, nucleiBox):
+    while True:
+        vorRegions = measure.regionprops(segmentedImg)
+        segmentedImg, corrected = connect_region(vorRegions, segmentedImg, nucleiBox)
+        if (not corrected):
+            break
     plt.imshow(segmentedImg)
     plt.show()
-    vorRegions = measure.regionprops(segmentedImg)
+    return segmentedImg
+        
+# This function is called by correct_voronoi. After this function reconnects a non-connected
+# voronoi segment to a different voronoi region, it will return with the updated segmentedImg.
+# This function returns, instead of continuing in the loop, as the segmentedImg must be
+# relabelled after each correction occurs.
+# Arguments:
+#   - vorRegions (list): list of attributes for each voronoi region
+#   - segmentedImg (numpy array): The voronoi segmented image.
+#   - nucleiBox (numpy array): The image of the nuclei for the image
+# Returns:
+#   - A tuple of segmentedImg and a bool: The bool represents whether the image has been
+#     corrected in this step. If it is corrected, then we call connect_region again. If it
+#     has not, then we do not need to call connect_region again, as this indicates that
+#     the image is already correct.
+def connect_region(vorRegions, segmentedImg, nucleiBox):
     for r in vorRegions:
-        # Below obtains the nucleus for this voronoi region. It will then be used
-        # as an intensity image to determine the regions that are not connected to this
-        # nucleus, to find the correct region that they are connected to.
-        intensityImage = (segmentedImg == r['label']).astype(int)
-        nucleiProps = measure.regionprops(nucleiBox,
-                                  intensity_image=intensityImage
-                                 )
-        # Obtain the nucleus with the highest intensity.
-        n = max(nucleiProps, key=lambda n:n.intensity_mean)
+        # First, obtain the regions that are not meant to be in the voronoi region
+        # defined by r.
+        separateRegs = obtain_separate_regions(segmentedImg, nucleiBox, r)
         
-        # rLabel is the labelling of the region defined by r.
-        # It will differentially label regions that are not connected.
-        # The shape of rLabel is segmentedImg.shape, so that we can record the exact
-        # positions of each label, and connect with other labels correctly.
-        rLabel = np.zeros(segmentedImg.shape)
-        rLabel[r.bbox[0]:r.bbox[2], r.bbox[1]:r.bbox[3]] = r.image
-        rLabel = measure.label(rLabel)
+        # Make a list of the regions other than the current one being analysed.
+        otherRegions = [vorRegion for vorRegion in vorRegions if not vorRegion.label == r.label]
         
-        # intensityImage will contain an image of the nucleus for the region defined
-        # by r. This will be used to extract out the regions that are not connected to this
-        # nucleus.
-        # Once again, we use nucleiBox.shape, so that we can use this as an intensity image
-        # with rLabel, noting that nucleiBox and segmentedImg have the same shape.
-        intensityImage = np.zeros(nucleiBox.shape)
-        intensityImage[n.bbox[0]:n.bbox[2], n.bbox[1]:n.bbox[3]] = n.image
+        # Then, attempt to connect the regions (defined by cellProps) to otherRegions.
+        # Reminder: sepReg is referring to the regions that are separated from the main region's
+        # nucleus. OtherReg is a cell in another voronoi region that sepReg might be connected to
+        # instead.
+        for sepReg in separateRegs:
+            sepRegImg = np.zeros(segmentedImg.shape)
+            # First, obtain the image of the separated region.
+            sepRegImg[sepReg.bbox[0]:sepReg.bbox[2], sepReg.bbox[1]:sepReg.bbox[3]] = sepReg.image
+            sepRegImg = np.where(sepRegImg == 1, r.label, sepRegImg)
+            for otherReg in otherRegions:
+                # Create a new image of the otherRegion.
+                otherRegImg = np.zeros(segmentedImg.shape)
+                otherRegImg[otherReg.bbox[0]:otherReg.bbox[2], otherReg.bbox[1]:otherReg.bbox[3]] =otherReg.image
+                # Set the labels of otherRegImg to be otherReg.label
+                otherRegImg = np.where(otherRegImg == 1, otherReg.label, otherRegImg)
+                # Label the otherRegImg. This will be used to compare whether the regions are connected.
+                # We need the number of regions from this labelled image.
+                otherRegLabel, otherRegNum = measure.label(otherRegImg, return_num=True)
+                # Now, connect the separated region and the other region.
+                # The separated region will have the same label as the other region,
+                # to allow measure.label to correctly label adjacent regions, in the case
+                # that the separated region is connected to the other region. If this is not
+                # done, then label will assume that adjacent regions with different
+                # labels will be two separate regions, rather than one single region.
+                connectImage = np.where(sepRegImg > 0, otherReg.label, otherRegImg)
+                connectLabel, numReg = measure.label(connectImage, return_num=True)
+                # Check if the resulting number of regions is the same as before.
+                if (numReg == otherRegNum):
+                    # If the number of labelled regions is the same as before the sepReg
+                    # was added on, then sepReg is connected to the new region.
+                    return (np.where(sepRegImg > 0, otherReg.label, segmentedImg), True)
+    # If we have not changed anything, return True in the second index of the tuple.
+    # This will stop the overarching while loop that is calling connect_region.
+    return (segmentedImg, False)
 
-        # The section below simply filters out the labelled region that contains the
-        # nucleus. The regions that are not connected to the cell (and therefore,
-        # not connected to the nucleus) remain. These regions are connected to a label
-        # in another voronoi region.
-        cellProps = measure.regionprops(rLabel, intensity_image=intensityImage)
-        cellProps = list(filter(filter_zero_mean_intensity, cellProps))
-        
-        for c in cellProps:
-            cool = np.zeros(segmentedImg.shape)
-            cool[c.bbox[0]:c.bbox[2], c.bbox[1]:c.bbox[3]] = c.image
-            plt.imshow(cool)
-            plt.show()
-            
-        # Now connect each of cellProps to a different labelled region in a different image.
-        # Then, label the image. Label this region, and record the number of regions before
-        # adding the labelled region. Once the labelled region is added,
-        # do another labelling, and if the number of regions remains constant,
-        # then the labelled region is connected correctly. (Note that I may need to change
-        # the label of the labelled region when I add it (to the label that we are adding to),
-        # as measure.label may treat them differently.)
-        # Then, simply take segmentedImg, and then copy the label over from one region
-        # to the current one that we are considering (need to relabel for the for loop).
-        # Make an overarching while loop then, for when segmentedImg is updated.
-        
+# This function obtains the separated regions in a voronoi region. A separated region
+# is a region that is not adjacent to the nucleus of the voronoi region.
+# I.e. it is not part of the main cell in this voronoi region, and instead, part of
+# another cell in another voronoi region.
+# Arguments:
+#   - segmentedImg (numpy array): The voronoi segmented image.
+#   - nucleiBox (numpy array): The image of the nuclei.
+#   - ogRegion (Dictionary): A list of information for the region that we are currently
+#                            scanning in.
+# Returns:
+#   - A list of the regions that are not touching the nuclei, separate to the 
+#     main label in the voronoi region.
+def obtain_separate_regions(segmentedImg, nucleiBox, ogRegion):
+    # Below obtains the nucleus for this voronoi region. It will then be used
+    # as an intensity image to determine the regions that are not connected to this
+    # nucleus, to find the correct region that they are connected to.
+    intensityImage = (segmentedImg == ogRegion['label']).astype(int)
+    nucleiProps = measure.regionprops(nucleiBox,
+                              intensity_image=intensityImage
+                             )
+    # Obtain the nucleus with the highest intensity.
+    n = max(nucleiProps, key=lambda n:n.intensity_mean)
+    
+    # rLabel is the labelling of the region defined by ogRegion.
+    # It will differentially label regions that are not connected.
+    # The shape of rLabel is segmentedImg.shape, so that we can record the exact
+    # positions of each label, and connect with other labels correctly.
+    rLabel = np.zeros(segmentedImg.shape)
+    rLabel[ogRegion.bbox[0]:ogRegion.bbox[2], ogRegion.bbox[1]:ogRegion.bbox[3]] = ogRegion.image
+    rLabel = measure.label(rLabel)
+    
+    # intensityImage will contain an image of the nucleus for the region defined
+    # by ogRegion. This will be used to extract out the regions that are not connected to this
+    # nucleus.
+    # Once again, we use nucleiBox.shape, so that we can use this as an intensity image
+    # with rLabel, noting that nucleiBox and segmentedImg have the same shape.
+    intensityImage = np.zeros(nucleiBox.shape)
+    intensityImage[n.bbox[0]:n.bbox[2], n.bbox[1]:n.bbox[3]] = n.image
 
-        
-        
-        
-        
-
-        
+    # The section below simply filters out the labelled region that contains the
+    # nucleus. The regions that are not connected to the cell (and therefore,
+    # not connected to the nucleus) remain. These regions are connected to a label
+    # in another voronoi region.
+    separateRegs = measure.regionprops(rLabel, intensity_image=intensityImage)
+    return list(filter(filter_zero_mean_intensity, separateRegs))
 
 if __name__ == '__main__':
     image_analysis("../Images/Nuclei", "../Images/Pathogen", "../Images/Cell", 10000, './temp')
